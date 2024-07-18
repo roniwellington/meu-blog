e203dd7ee324c1f14b442c1765ed3c06dc1948d4
roniwellington <roniwellington@gmail.com>
2024-07-18 18:30:19 -0300

    Refatoração e validação, Associando tabelas e mensagens de erro

--- usuarios/views.py
+++ usuarios/views.py
@@ -37,9 +37,7 @@ def cadastro(request):
     if request.method == 'POST':
         form = CadastroForms(request.POST)
         if form.is_valid():
-            if form["senha_1"].value() != form["senha_2"].value():
-                messages.error(request, "Senhas não são iguais")
-                return redirect('cadastro')
+            
             
             nome=form["nome_cadastro"].value()
             email=form["email"].value()
