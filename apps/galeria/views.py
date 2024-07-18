cc5833d66cbb88d5e7674ed77fa9130e01aecac0
roniwellington <roniwellington@gmail.com>
2024-07-18 20:58:50 -0300

    adicionando crud na aplicação(inserir, editar e deletar uma imagem/conteudo)

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
 from django.contrib import messages
 
 
@@ -26,3 +27,27 @@ def buscar(request):
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
     return render(request, 'galeria/buscar.html', {"cards": fotografias})
+
+
+
+def nova_imagem(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "usuário não logado")
+        return redirect('login')
+    
+    form = FotografiaForms
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Nova fotografia cadastrada!')
+            return redirect('index')
+        
+        
+    return render(request, 'galeria/nova_imagem.html', {'form': form})
+
+def editar_imagem(request):
+    pass
+
+def deletar_imagem(request):
+    pass
